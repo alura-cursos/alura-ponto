708f59766e274502948dae2c71ae7b6541dc84f3
Alex Felipe <alexfelipevieira@gmail.com>
2021-10-26 08:26:17 -0300

    initial project

---
+++ src/main/kotlin/br/com/alura/aluraponto/AluraPontoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.alura.aluraponto
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AluraPontoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AluraPontoApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/alura/aluraponto/AluraPontoApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.alura.aluraponto
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AluraPontoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
